ae69ef385c46b74323f24f6d1f1861813def9726
mastermeric <mastermeric@gmail.com>
2023-05-07 13:38:55 +0300

    functional operations are added

--- src/main/java/com/live_scoreboard/football_worldcup/controller/FootballController.java
+++ src/main/java/com/live_scoreboard/football_worldcup/controller/FootballController.java
@@ -1,9 +1,13 @@
 package com.live_scoreboard.football_worldcup.controller;
 
+import java.util.List;
+
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.live_scoreboard.football_worldcup.GlobalFootballVariables;
+import com.live_scoreboard.football_worldcup.model.FootballModel;
 import com.live_scoreboard.football_worldcup.service.FootballService;
 
 public class FootballController {
@@ -15,12 +19,39 @@ public class FootballController {
             _footballService = footballService; // Start with Injecting loosly coupled service..
         }
         //---------------------------------------------------------------------------
+        
+        //Simple HealthCheck endpoint -----------------------------------------------
+        @GetMapping("/api/healthCheck")
+        public @ResponseBody String HealthCheck() {
+            return GlobalFootballVariables.SuccessServerMessage;
+        }
+        //---------------------------------------------------------------------------
 
-    
-    //Simple HealthCheck endpoint just to see Server is Healthy -----------------
-    @GetMapping("/api/healthCheck")
-    public @ResponseBody String HealthCheck() {
-        return GlobalFootballVariables.SuccessServerMessage;
-    }
-    //---------------------------------------------------------------------------
+
+        //Basic operations : Functional game operations  ----------------------------        
+        @GetMapping(GlobalFootballVariables.EndPoint_StartNewGame)
+        public @ResponseBody String StartNewGame(String HomeTeamName , String AwayTeamName) {
+            String res = _footballService.StartNewGame(HomeTeamName, AwayTeamName);
+            return res;
+        }
+
+        @GetMapping(GlobalFootballVariables.EndPoint_UpdateScore)
+        public @ResponseBody String UpdateScore(String HomeTeamName , String AwayTeamName, Integer newScoreToHomeTeam, Integer newScoreToAwayTeam) {
+            String res = _footballService.UpdateScore(HomeTeamName, AwayTeamName, newScoreToHomeTeam, newScoreToAwayTeam);
+            return res;
+        }
+
+        @GetMapping(GlobalFootballVariables.EndPoint_FinishGame)
+        public @ResponseBody String FinishGame(String HomeTeamName , String AwayTeamName) {
+            String res = _footballService.FinishGame(HomeTeamName, AwayTeamName);
+            return res;
+        }
+
+        @GetMapping(GlobalFootballVariables.EndPoint_GetSummary)
+        public @ResponseBody String GetSummary() {
+            List<FootballModel> model = _footballService.getGameSummary();
+            JSONObject jo = new JSONObject(model);
+            return jo.toString();
+        }
+        //---------------------------------------------------------------------------
 }
--- src/test/java/com/live_scoreboard/football_worldcup/FootballWorldcupApplicationTests.java
+++ src/test/java/com/live_scoreboard/football_worldcup/FootballWorldcupApplicationTests.java
@@ -15,12 +15,7 @@ class FootballWorldcupApplicationTests {
 	@Autowired
 	MockMvc mockMvc;
 
-	// //TEST-0 : Server Module is Loaded Or Not.
-	// @Test
-	// void contextLoads() throws Exception {
-	// }
-
-
+	//=================   SERVER BASED TESTS  ======================
 	//TEST-1 : Simple Server Healthcheck is OK.
 	@Test
 	void should_Success_When_Server_Communication_Is_OK() throws Exception {		
@@ -33,15 +28,20 @@ class FootballWorldcupApplicationTests {
 	@Test
 	void should_Fail_With_Wrong_EndPoints() throws Exception {		
 		
-		//Failed URL to see 404 Not Found error ..
+		//Dummy URL to see 404 Not Found error ..
 		String wrongRequest ="/api/nonExistingEndpoint_DummyUrl"; 
 
 		mockMvc.perform(get(wrongRequest))			
 			.andExpect(status().isOk()) //Continue if we have 200 OK (successfull).
 			.andExpect(content().string(GlobalFootballVariables.SuccessServerMessage));  // Modify This string To see Expected/Unexpected result comes.
 	}
+	//=============================================================
 
-	//TEST-3 : Start Game  => To see if Game can be started successfully.
+
+
+	//=================   FUNCTIONAL TESTS  =======================
+
+	//TEST-3 : Start Game 
 	@Test
 	void should_Success_When_Game_Is_Started() throws Exception {		
 		mockMvc.perform(get(GlobalFootballVariables.EndPoint_StartNewGame))			
@@ -49,7 +49,15 @@ class FootballWorldcupApplicationTests {
 			.andExpect(content().string(GlobalFootballVariables.SuccessServerMessage));  // Modify This string To see Expected/Unexpected result comes.
 	}
 
-	//TEST-4 : Finish Game  => To see if Game can be finished successfully.
+	//TEST-4 : Update Game 
+	@Test
+	void should_Success_When_Score_Is_Updated() throws Exception {		
+		mockMvc.perform(get(GlobalFootballVariables.EndPoint_UpdateScore))			
+			.andExpect(status().isOk()) //Continue if we have 200 OK (successfull).
+			.andExpect(content().string(GlobalFootballVariables.SuccessServerMessage));  // Modify This string To see Expected/Unexpected result comes.
+	}
+
+	//TEST-5 : Finish Game 
 	@Test
 	void should_Success_When_Game_Is_Finished() throws Exception {		
 		mockMvc.perform(get(GlobalFootballVariables.EndPoint_FinishGame))			
@@ -57,11 +65,12 @@ class FootballWorldcupApplicationTests {
 			.andExpect(content().string(GlobalFootballVariables.SuccessServerMessage));  // Modify This string To see Expected/Unexpected result comes.
 	}
 
-	//TEST-5 : Get Summary  => To see Scoreboard results successfully retrieved.
+	//TEST-6 : Get Summary of scores for the games that are in-progess
 	@Test
 	void should_Success_When_Scoreboard_Results_Retrieved() throws Exception {		
 		mockMvc.perform(get(GlobalFootballVariables.EndPoint_GetSummary))			
 			.andExpect(status().isOk()) //Continue if we have 200 OK (successfull).
 			.andExpect(content().string(GlobalFootballVariables.SuccessServerMessage));  // Modify This string To see Expected/Unexpected result comes.
 	}
+	//=============================================================
 }
