41c426725c47c206b4e0becb98af78d7271b22e5
mastermeric <mastermeric@gmail.com>
2023-05-09 00:56:34 +0300

    additinal fixes for Test flow and Sorting logic

--- src/main/java/com/live_scoreboard/football_worldcup/controller/FootballController.java
+++ src/main/java/com/live_scoreboard/football_worldcup/controller/FootballController.java
@@ -1,77 +1,100 @@
 package com.live_scoreboard.football_worldcup.controller;
 
 import java.util.List;
-
-import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.live_scoreboard.football_worldcup.GlobalFootballVariables;
-import com.live_scoreboard.football_worldcup.model.FootballModel;
 import com.live_scoreboard.football_worldcup.service.FootballService;
 
 
 @RestController
 public class FootballController {
         private FootballService _footballService = new FootballService();        
-
-        /*
-        //TEST code will detect errors if we do mistakes in constructor -------------
-        FootballController(FootballService footballService) {
-            _footballService = footballService; // Start with Injecting loosly coupled service..
-        }
-        //---------------------------------------------------------------------------
-        */
         
-        //Simple HealthCheck endpoint -----------------------------------------------
+        
+        //Simple HealthCheck endpoint ==============================================
         @GetMapping(GlobalFootballVariables.EndPoint_HealthCheck)
-        public String HealthCheck() {
-            return GlobalFootballVariables.SuccessServerMessage;
+        public ResponseEntity<String> HealthCheck() {
+            try {
+                return ResponseEntity.status(HttpStatus.OK).body(GlobalFootballVariables.SuccessServerMessage);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
+            }            
         }
-        //---------------------------------------------------------------------------
-
+        //==========================================================================
 
         
 
-        //Basic operations : Functional game operations  ----------------------------        
+        //Basic game operations  ===================================================
         @GetMapping( GlobalFootballVariables.EndPoint_StartNewGame + "/{HomeTeamName}/{AwayTeamName}")
-        public @ResponseBody String StartNewGame( 
+        public ResponseEntity<String> StartNewGame( 
             @PathVariable("HomeTeamName") String HomeTeamName 
             ,@PathVariable("AwayTeamName") String AwayTeamName)
             {
-                String res = _footballService.StartNewGame(HomeTeamName, AwayTeamName);
-                return res;
+                try {
+                    String res = _footballService.StartNewGame(HomeTeamName, AwayTeamName);
+
+                    if(res.contains("Error")) {
+                        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(res);
+                    }
+
+                    return ResponseEntity.status(HttpStatus.OK).body(res);
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());
+                }
             }
 
         @GetMapping(GlobalFootballVariables.EndPoint_UpdateScore+ "/{HomeTeamName}/{AwayTeamName}/{newScoreToHomeTeam}/{newScoreToAwayTeam}")
-        public @ResponseBody String UpdateScore(
+        public ResponseEntity<String> UpdateScore(
             @PathVariable("HomeTeamName") String HomeTeamName 
             , @PathVariable("AwayTeamName") String AwayTeamName
             , @PathVariable("newScoreToHomeTeam") Integer newScoreToHomeTeam
-            , @PathVariable("newScoreToAwayTeam") Integer newScoreToAwayTeam) 
-
+            , @PathVariable("newScoreToAwayTeam") Integer newScoreToAwayTeam)
             {
-                String res = _footballService.UpdateScore(HomeTeamName, AwayTeamName, newScoreToHomeTeam, newScoreToAwayTeam);
-                return res;
-        }
+                try {
+                    String res = _footballService.UpdateScore(HomeTeamName, AwayTeamName, newScoreToHomeTeam, newScoreToAwayTeam);
+
+                    if(res.contains("Error")) {
+                        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(res);
+                    }
+
+                    return ResponseEntity.status(HttpStatus.OK).body(res);
+                } catch (Exception e) {                    
+                    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());   
+                }                
+            }
 
         @GetMapping(GlobalFootballVariables.EndPoint_FinishGame + "/{HomeTeamName}/{AwayTeamName}")
-        public @ResponseBody String FinishGame(
+        public ResponseEntity<String> FinishGame(
             @PathVariable("HomeTeamName") String HomeTeamName 
             ,@PathVariable("AwayTeamName") String AwayTeamName) 
             {
-                String res = _footballService.FinishGame(HomeTeamName, AwayTeamName);
-                return res;
+                try {
+                    String res = _footballService.FinishGame(HomeTeamName, AwayTeamName);
+
+                    if(res.contains("Error")) {
+                        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(res);
+                    }
+
+                    return ResponseEntity.status(HttpStatus.OK).body(res);
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(e.getMessage());   
+                }                
             }
 
         @GetMapping(GlobalFootballVariables.EndPoint_GetSummary)
-        public @ResponseBody List<FootballModel> GetSummary() {
-            List<FootballModel> model = _footballService.getGameSummary();
-            return model;
+        public ResponseEntity<List<String>> GetSummary() {
+            
+            try {                
+                List<String> scoreBoardList = _footballService.getGameSummary();                
+                return ResponseEntity.status(HttpStatus.OK).body(scoreBoardList);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);   
+            }            
         }
-        //---------------------------------------------------------------------------
+        //===========================================================================
         
 }
